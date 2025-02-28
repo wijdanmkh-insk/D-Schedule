a74e286a432c26025bf72be035e48a75642e0699
wijdan <wijdaninsaniak@gmail.com>
2025-02-28 17:55:34 +0700

    adding features to work on

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ typedef struct user{
     char name[100];
     char username[100];
     char password[100];
+    char date[20];
+    int attendance;
     int charge;
     int balance;
 }user;
@@ -16,22 +18,30 @@ void sort(user *users, int max);
 
 
 // ADMIN OR EVALUATOR
-void signin_admin();
-void menu_admin();
+void signin_admin(); //
+void menu_admin(); //
 void attendance_check();
 void give_charge();
 void see_users();
+void delete_users();
 
 //USER
 void signup_signin_user();
-void menu_user(char *username);
-void change_password(char *username);
-void check_balance(char *username);
+void menu_user(char *username, char *name);
+void change_password(char *username, char *name);
+void check_balance(char *username, char *name);
+void seek_charges(char *username, char *name);
+void delete_charges(char *username, char *name);
+
+//FILE HANDLING
+FILE* data_file;
+FILE *attendance_file;
+FILE* charge_file;
 
 int main(){
     main_menu();
 }
-
+//UNIVERSAL FUNCTIONS
 void main_menu(){
     printf("================ MAIN MENU ===================\n");
     printf("Hello! Choose the role to use the app :\n");
@@ -52,8 +62,9 @@ void main_menu(){
 
 }
 
+//================================================================================
+//USER MENU
 void signup_signin_user(){
-    FILE* data_file;
     user data;
     user find;
 
@@ -120,7 +131,7 @@ void signup_signin_user(){
             if (found) {
                 printf("Login successful! Press any key to continue...\n");
                 getchar();
-                menu_user(data.username);
+                menu_user(data.username, find.name);
                 break;
             } else {
                 printf("Wrong username or password! Remaining attempts: %d\n", trial - 1);
@@ -137,52 +148,35 @@ void signup_signin_user(){
     }
 }
 
-void signin_admin(){
-    user data;
-
-    for(int trial = 3; trial > 0; --trial){
-        if(trial > 0){
-            printf("================ ADMIN LOGIN ================\n");
-            printf("Before login, please input few informations to begin\n");
-            printf("Username : "); gets(data.username);
-            printf("Password : "); gets(data.password);
-
-                if(strcmp(data.username, "admin")== 0 && strcmp(data.password, "admin") == 0){
-                    printf("login success! press any key to continue..."); getchar();
-                    menu_admin();
-                }else{
-                    printf("Remaining account input : %d\n", trial-1);
-                    printf("wrong username or password! try again!\n");
-                }
-        }else{
-            printf("login failed! press any key to continue..."); getchar();
-            main_menu();
-        }
-    }
-}
-
-//================================================================================
-//USER MENU AND FUNCTIONS
-void menu_user(char *username){
+void menu_user(char *username, char *name){
     printf("============== USER MENU =============\n");
-    printf("1. Check for balance\n2. Change Password\n3. Log Out\n4. Exit\nChoose : "); int choose; scanf("%d", &choose); getchar();
+    printf("1. Check for balance\n2. Change Password\n3. See Charges\n4. Leaderboards\n5. Pay Charge\n6. Log Out\n7. Exit\nChoose : "); int choose; scanf("%d", &choose); getchar();
 
     switch(choose){
         case 1:
-            check_balance(username);
+            check_balance(username, name);
             break;
         case 2:
-            change_password(username);
+            change_password(username, name);
             break;
         case 3:
-            main_menu();
+            see_charges(username, name);
             break;
         case 4:
+            see_users(username, name);
+            break;
+        case 5: 
+            delete_charges(username, name);
+            break;
+        case 6:
+            main_menu();
+            break;
+        case 7:
             exit(0);
             break;
         default:
             printf("Wrong choice! Try again!");
-            menu_user(username);
+            menu_user(username, name);
     }
 }
 
@@ -206,10 +200,10 @@ void check_balance(char *username, char *name){
     printf("\nPress any key to continue...");
     getchar();
 
-    menu_user(username);
+    menu_user(username, name);
 }
 
-void change_password(char *username){
+void change_password(char *username, char *name){
    user change, find;
    int found;
    char password[100];
@@ -239,11 +233,93 @@ void change_password(char *username){
    }
 
    printf("Press any key to continue..."); getchar();
-   menu_user(username);
+   menu_user(username, name);
+}
+
+void delete_charge(char *username, char *name) {
+    FILE *user_data, *temp_data;
+    user read;
+    int found = 0;
+
+    printf("============= DELETE CHARGE =============\n");
+
+    user_data = fopen("data_user.dat", "rb");
+    if (user_data == NULL) {
+        printf("Error: User data file not found.\n");
+        return;
+    }
+
+    temp_data = fopen("temp.dat", "wb");
+    if (temp_data == NULL) {
+        printf("Error: Unable to create temp file.\n");
+        fclose(user_data);
+        return;
+    }
+
+    while (fread(&read, sizeof(read), 1, user_data)) {
+        if (strcmp(read.username, username) == 0) {
+            found = 1;
+            printf("Current Balance: Rp%d\n", read.balance);
+            printf("Current Charge: Rp%d\n", read.charge);
+
+            if (read.charge > 0) {
+                read.balance -= read.charge; // Deduct charge from balance
+                if (read.balance < 0) read.balance = 0; // Prevent negative balance
+                read.charge = 0; // Reset charge
+
+                printf("Charge deleted. Updated Balance: Rp%d\n", read.balance);
+            } else {
+                printf("No charge to delete.\n");
+            }
+        }
+        fwrite(&read, sizeof(read), 1, temp_data);
+    }
+
+    fclose(user_data);
+    fclose(temp_data);
+
+    if (found) {
+        remove("data_user.dat");
+        rename("temp.dat", "data_user.dat");
+        printf("Charge deletion successful!\n");
+    } else {
+        remove("temp.dat");
+        printf("User not found.\n");
+    }
+
+    printf("Press any key to continue...");
+    getchar();
+    menu_user(username, name);
 }
 
 
+
 //ADMIN MENU
+void signin_admin(){
+    user data;
+
+    for(int trial = 3; trial > 0; --trial){
+        if(trial > 0){
+            printf("================ ADMIN LOGIN ================\n");
+            printf("Before login, please input few informations to begin\n");
+            printf("Username : "); gets(data.username);
+            printf("Password : "); gets(data.password);
+
+                if(strcmp(data.username, "admin")== 0 && strcmp(data.password, "admin") == 0){
+                    printf("login success! press any key to continue..."); getchar();
+                    system("cls");
+                    menu_admin();
+                }else{
+                    printf("Remaining account input : %d\n", trial-1);
+                    printf("wrong username or password! try again!\n");
+                }
+        }else{
+            printf("login failed! press any key to continue..."); getchar();
+            main_menu();
+        }
+    }
+}
+
 void menu_admin(){
     printf("============== ADMIN MENU =============\n");
     printf("1. Check Attendance\n2. Give Charge\n3. See User Lists\n4. Log Out\n5. Exit\nChoose : "); int choose; scanf("%d", &choose); getchar();
@@ -270,28 +346,96 @@ void menu_admin(){
     }
 }
 
-void attendance_check(){
-    printf("================= ATTENDANCE CHECK =================\n");
-    printf("This is the menu of attendance check, to make sure\n");
-    printf("the employees are coming to work on time. Please add the\n");
-    printf("time limit of attendance below, so the system can check\n");
-    printf("the time as soon as possible.\n");
+void attendance_check() {
+    char search[100], date[20];
+    struct user find;
+    int found = 0, attendance_status;
 
-    printf("press any key to continue..."); getchar();
+    printf("================= ATTENDANCE CHECK =================\n");
+    printf("Input the current date: "); gets(date);
+    printf("Input the employee's name to check attendance: "); gets(search);
+
+    attendance_file = fopen("user_attendance.dat", "rb+");
+    if (attendance_file == NULL) {
+        attendance_file = fopen("user_attendance.dat", "ab+");
+    }
+
+    while (fread(&find, sizeof(find), 1, attendance_file)) {
+        if (strcmp(search, find.name) == 0 && strcmp(date, find.date) == 0) {
+            found = 1;
+            break;
+        }
+    }
+
+    if (found) {
+        printf("Attendance for %s on %s is currently: %s\n", find.name, find.date, 
+               find.attendance ? "Present" : "Absent");
+        printf("Update attendance? Present (1) / Absent (0): ");
+        scanf("%d", &attendance_status);
+        getchar(); // Consume newline to avoid issues with gets()
+
+        find.attendance = attendance_status;
+        
+        // Move file pointer back to overwrite the correct record
+        fseek(attendance_file, -sizeof(find), SEEK_CUR);
+        
+        if (fwrite(&find, sizeof(find), 1, attendance_file) == 1) {
+            printf("Data updated successfully!\n");
+        } else {
+            printf("Error updating data.\n");
+        }
+    } else {
+        printf("User not found for the given date...\n");
+    }
+
+    printf("Press any key to continue..."); getchar();
+    fclose(attendance_file);
     menu_admin();
 }
 
-void give_charge(){
-    FILE *user_data;
-    user_data = fopen("data_user.dat", "rb+");
+void give_charge() {
+    char search[100], date[20];
+    struct user find;
+    int found = 0, new_charge;
 
-    printf("=================== GIVE CHARGE ==================\n");
-    printf("This is the menu of giving charge for employees that\n");
-    printf("late exceeding the limit time of being late. Make sure\n");
-    printf("that you've been inputting the right username\n");
+    printf("================= GIVE CHARGE =================\n");
+    printf("Input the current date: "); gets(date);
+    printf("Input the employee's name to update charge: "); gets(search);
 
-    fclose(user_data);
-    printf("press any key to continue..."); getchar();
+    charge_file = fopen("user_charge.dat", "rb+");
+    if (charge_file == NULL) {
+        charge_file = fopen("user_charge.dat", "ab+");
+    }
+
+    while (fread(&find, sizeof(find), 1, charge_file)) {
+        if (strcmp(search, find.name) == 0 && strcmp(date, find.date) == 0) {
+            found = 1;
+            break;
+        }
+    }
+
+    if (found) {
+        printf("Current charge for %s on %s: %d\n", find.name, find.date, find.charge);
+        printf("Enter new charge: ");
+        scanf("%d", &new_charge);
+        getchar(); // Consume newline to avoid issues with gets()
+
+        find.charge = new_charge;
+
+        // Move file pointer back to overwrite the correct record
+        fseek(charge_file, -sizeof(find), SEEK_CUR);
+        
+        if (fwrite(&find, sizeof(find), 1, charge_file) == 1) {
+            printf("Charge updated successfully!\n");
+        } else {
+            printf("Error updating charge.\n");
+        }
+    } else {
+        printf("User not found for the given date...\n");
+    }
+
+    printf("Press any key to continue..."); getchar();
+    fclose(charge_file);
     menu_admin();
 }
 
@@ -317,7 +461,7 @@ void see_users(){
     
     int choose;
     while(choose < 1 || choose > 2){
-        printf("choose assorted data from : 1. Lowest - highest\n2. Highest - Lowest\n Choose : "); scanf("%d", &choose); getchar();
+        printf("choose assorted data from :\n1. Lowest - highest\n2. Highest - Lowest\n Choose : "); scanf("%d", &choose); getchar();
         switch (choose) {
             case 1:
                 printf("\nDisplaying data (Lowest to Highest):\n");
@@ -345,6 +489,53 @@ void see_users(){
     printf("press any key to continue..."); getchar();
     menu_admin();
 }
+
+void delete_users() {
+    FILE *user_data, *temp_data;
+    user read;
+    char target_name[100];
+    int found = 0;
+
+    printf("================= DELETE USER =================\n");
+    printf("Enter the name of the user to delete: "); gets(target_name);
+
+    user_data = fopen("data_user.dat", "rb");
+    if (user_data == NULL) {
+        printf("Error: User data file not found.\n");
+        return;
+    }
+
+    temp_data = fopen("temp.dat", "wb");
+    if (temp_data == NULL) {
+        printf("Error: Unable to create temp file.\n");
+        fclose(user_data);
+        return;
+    }
+
+    while (fread(&read, sizeof(read), 1, user_data)) {
+        if (strcmp(read.name, target_name) == 0) {
+            found = 1; // Mark as found but don't write to temp file
+        } else {
+            fwrite(&read, sizeof(read), 1, temp_data);
+        }
+    }
+
+    fclose(user_data);
+    fclose(temp_data);
+
+    if (found) {
+        remove("data_user.dat");     // Delete original file
+        rename("temp.dat", "data_user.dat"); // Rename temp file
+        printf("User '%s' deleted successfully!\n", target_name);
+    } else {
+        remove("temp.dat"); // No need to keep temp file if no changes were made
+        printf("User '%s' not found.\n", target_name);
+    }
+
+    printf("Press any key to continue..."); getchar();
+    menu_admin();
+}
+
 
 void sort(struct user *users, int max) {
     struct user temp;
